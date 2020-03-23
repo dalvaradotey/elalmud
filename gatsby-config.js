2db6340deb85e3d203e62be0d30063c4c1f8a47a
Diego Alvarado Otey <diego@Diegos-MacBook-Pro.local>
2020-03-22 23:28:26 -0300

    FIX: google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,15 @@ module.exports = {
     author: `@dalvaradotey`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-161586090-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -49,26 +58,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-modal-routing`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-161586090-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "elalmud.com",
-      },
-    },
   ],
 }
