8eaa74e625538e6714b1ac78e89482fa5ea6fe6c
Diego Alvarado Otey <diego@Diegos-MacBook-Pro.local>
2020-03-22 23:22:04 -0300

    FIX: analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "elalmud.com",
       },
     },
   ],
